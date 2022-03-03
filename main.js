0f92fdcf6126e6a1a9efc7b939f054ffd9d3d4a4
anishthota12 <80860095+anishthota12@users.noreply.github.com>
2022-03-02 19:22:20 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+leftWristX = 0;
+rightWristX = 0;
+difference = 0;
+
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+    video.position(50, 240);
+
+    canvas = createCanvas(550, 500);
+    canvas.position(screen.width - 600, 240);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses);
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = Math.floor(leftWristX - rightWristX);
+        console.log("Left Wrist X: " + Math.round(leftWristX) + ", Right Wrist X: " + Math.round(rightWristX));
+    }
+}
+
+function modelLoaded() {
+    console.log("Posenet is Initialized");
+}
+
+function draw() {
+    background("#FFF");
+    textSize(difference / 1.5); // I divided by 1.5 to make the text fit inside the canvas
+    textFont('fontItalic');
+    text("Anish", 0, 325);
+    fill("darkblue");
+}
